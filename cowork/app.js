7342abf51f616680055813b784603cb12146cd95
ABiteOfSorrow <laceican@naver.com>
2023-03-13 19:01:45 +0100

    simple test for using express

--- cowork/app.js
+++ cowork/app.js
@@ -2,8 +2,23 @@ const express =require('express');
 
 const app = express();
 
-app.get('/hello', (req, res) => {
-    res.send('<h1>Hello Express</h1>');
+const members = require('./members');
+
+app.get('/api/members', (req, res) => {
+    res.send(members);
+});
+
+app.get('/api/members/:id', (req, res) => {
+    // const id = req.params.id;
+    const { id } = req.params;
+    const member = members.find((e) => e.id === Number(id));
+
+    if (member) {
+        res.send(member);
+    } else {
+        // if there isn't member header's status : 404
+        res.status(404).send({ message : 'There is no such member' });
+    }
 });
 
 app.listen(3000, () => {
